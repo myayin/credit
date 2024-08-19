8eaa129da154ae023d8a9bd5038f53d5bf1a6bde
MerveYayın <merve.yayin@kgteknoloji.com>
2024-08-19 08:23:54 +0300

    Fix bugs in GetCreditsByUserResponse

--- src/main/java/com/colendi/credit/service/impl/credit/CreditSpecification.java
+++ src/main/java/com/colendi/credit/service/impl/credit/CreditSpecification.java
@@ -6,8 +6,10 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -22,10 +24,13 @@ public class CreditSpecification implements Specification<Credit> {
 
         filter.getStatus().ifPresent(status -> predicates.add(builder.equal(root.get("status"), status)));
 
-        filter.getCreatedDate()
-                .ifPresent(createdDate -> predicates.add(builder.equal(root.get("created_date"), createdDate)));
+        filter.getCreatedDate().ifPresent(createdDate -> predicates.add(builder.equal(root.get("created_date"), createdDate)));
 
         filter.getUserId().ifPresent(userId -> predicates.add(builder.equal(root.get("user").get("id"), userId)));
+
+        if (query != null) {
+            query.orderBy(builder.asc(root.get("createdDate")));
+        }
 
         return builder.and(predicates.toArray(new Predicate[0]));
     }
